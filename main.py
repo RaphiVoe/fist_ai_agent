deee4ae42a09d171dbf39c3098598003a17a971a
RaphiVoe <raphael.voetterl.94@gmail.com>
2025-11-13 21:00:04 +0100

    **Enhance main.py to support user-provided prompts and verbose mode** - Added command-line argument parsing for custom prompts and verbosity. - Updated `genai.Client` to use structured message types. - Improved token usage logging with conditional verbose output.

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
     print("Hello from fist-ai-agent!")
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
+    if len(sys.argv) < 1:
+        print("Usage: python3 main.py <prompt>")
+        return sys.exit(1)
+    user_prompt = sys.argv[1]
+    verbose = True if len(sys.argv) > 2 and sys.argv[2] == "--verbose" else False
+
+    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
+
     response = client.models.generate_content(model="gemini-2.0-flash-001",
-                                   contents=prompt)
+                                              contents=messages)
+
     prompt_tokens = response.usage_metadata.prompt_token_count
     response_tokens = response.usage_metadata.candidates_token_count
+
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
     print(response.text)
-    print(f"Prompt tokens: {prompt_tokens}")
-    print(f"Response tokens: {response_tokens}")
+    if verbose:
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
 
 
 if __name__ == "__main__":
